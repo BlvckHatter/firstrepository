3997923492c1cf78f54cc3394dff1dbc061012f4
BlvckHatter <102858198+BlvckHatter@users.noreply.github.com>
2022-04-02 18:18:06 +0200

    Create firstpython.py
    equation test with variables

---
+++ firstpython.py
@@ -0,0 +1,14 @@
+#Display the output
+
+print('Hello my fellow data scientist')
+print('Welcome to wonderland')
+
+print('Lets find the combo total price')
+
+brasato=12,50
+gnocchi=10
+tiramisu=8
+
+x=brasato+gnocchi+tiramisu
+
+print=x
